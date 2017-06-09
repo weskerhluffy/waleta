b4fdfb308769eef4cd045262e5121c15d03d2519
Darkly Dreaming Wesker <weskerhluffy@gmail.com>
2017-06-09 06:20:48 -0500

    empezado ejer 3

--- src/exercise_2.sql
+++ src/exercise_2.sql
@@ -8,30 +8,40 @@ begin
   declare spaces_present int;
   declare char_to_replace varchar(1);
   
+  set input_str_int=trim(lower(input_str));
   set input_str_len=length(input_str_int);
   
-  if(input_str_len = 0 or (input_str REGEXP '[[:alpha:]]') = 0) then
-  	return input_str;
+  if(input_str_len = 0 or (input_str REGEXP '[[:alpha:]]*') = 0) then
+    return input_str;
   end if;
   
-  set input_str_int=lower(input_str);
-  
-  set spaces_present=instr(input_str_int, ' ');
-  while (spaces_present <> 0) do
+--  set spaces_present=instr(input_str_int, ' ');
+--  while (spaces_present <> 0) do
+  set spaces_present=0;
+  repeat
     if(spaces_present+1 <= input_str_len) then
       set char_to_replace = substring(input_str_int, spaces_present+1,1);
       if((char_to_replace REGEXP '[[:alpha:]]') <> 0) then
-	      set char_to_replace = upper(char_to_replace);
-	      set input_str_int=insert(input_str_int,spaces_present+1,1,char_to_replace);
+        set char_to_replace = upper(char_to_replace);
+        set input_str_int=insert(input_str_int,spaces_present+1,1,char_to_replace);
       end if;
+      set spaces_present=locate(' ',input_str_int,spaces_present+1);
+    else
+      set spaces_present=0;
     end if;
-    set spaces_present=locate(' ',input_str_int,spaces_present+1);
-  end while;
+  until spaces_present = 0
+  end repeat;
+--  end while;
   
   return input_str_int;
 end;
 $$
 delimiter ;
 
-select initcap('earthfeef');
-select initcap('earth feef');
+select initcap('earthfeef') = 'Earthfeef';
+select initcap('earth feef') = 'Earth Feef';
+select initcap('&arth feef ') = '&arth Feef';
+select initcap(' r$"%$·&arth     feef ') = 'R$"%$·&arth     Feef';
+select initcap(' $"%$·&arth     feef ') = '$"%$·&arth     Feef';
+select initcap(' $"%$  ·&arth     feef ') = '$"%$  ·&arth     Feef';
+select strcmp(initcap(' $"%$  ·&arth     feef '), '$"%$  ·&Arth     Feef')==0;
---
+++ src/exercise_3.sql
@@ -0,0 +1,56 @@
+-- CREATE TABLE sometbl ( ID INT, NAME VARCHAR(50) );
+-- INSERT INTO sometbl VALUES (1, 'Smith'), (2, 'Julio|Jones|Falcons'), (3, 'White|Snow'), (4, 'Paint|It|Red'), (5, 'Green|Lantern'), (6, 'Brown|bag');
+
+drop procedure if exists split_column;
+DELIMITER //
+CREATE PROCEDURE split_column 
+(IN con CHAR(20))
+BEGIN
+  DECLARE v_finished INTEGER DEFAULT 0;
+  DECLARE v_id int DEFAULT 0;
+  DECLARE v_name varchar(50) DEFAULT 0;
+  DECLARE previous_pos int DEFAULT 0;
+  DECLARE current_pos int DEFAULT 0;
+  DECLARE finished_cur_split boolean;
+  DECLARE v_name_len int DEFAULT 0;
+  DECLARE cur_split_len int DEFAULT 0;
+  DECLARE cur_split varchar(50);
+  DECLARE sc_cursor CURSOR FOR
+    SELECT s.id, s.name from sometbl s;
+  DECLARE CONTINUE HANDLER FOR NOT FOUND SET v_finished = 1;  
+  OPEN sc_cursor;
+ 
+  get_column: LOOP
+ 
+    FETCH sc_cursor INTO v_id, v_name;
+ 
+    IF v_finished = 1 THEN
+      LEAVE get_column;
+    END IF;
+    
+    set finished_cur_split = 0;
+    set previous_pos = 0;
+    set current_pos = 0;
+    set v_name_len=length(v_name);
+    repeat
+      set current_pos = locate('|',v_name,current_pos+1);
+      if(current_pos = 0) then
+        set finished_cur_split = 1;
+        set current_pos=v_name_len;
+        set cur_split_len=current_pos-previous_pos;
+      else
+        set cur_split_len=current_pos-previous_pos-1;
+      end if;
+      set cur_split=substring(v_name,previous_pos+1,cur_split_len);
+      select cur_split;
+      set previous_pos=current_pos;
+    until finished_cur_split
+    end repeat;
+    
+  END LOOP get_column;
+ 
+  CLOSE sc_cursor;
+END //
+DELIMITER ;
+
+call split_column ("oi");
