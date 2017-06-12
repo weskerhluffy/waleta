29268e3a358e3cfe163472c3d6a67f1424dd95cc
Darkly Dreaming Wesker <weskerhluffy@gmail.com>
2017-06-11 21:41:00 -0500

    hecho palindromo
    aunkeno del todo bien, timeout

--- src/exercise_4.sql
+++ src/exercise_4.sql
@@ -8,10 +8,13 @@
 --);
 
 --insert into bugs values (NULL, '2012-01-01','2012-01-02',10);
+insert into bugs values (NULL, '2012-01-21',null,10);
+insert into bugs values (NULL, '2012-01-11','2012-01-27',10);
+insert into bugs values (NULL, '2010-11-11','2010-11-29',10);
 
 
-set @range_begin = '2012-11-01';
-set @range_end= '2012-12-02';
+set @range_begin = '2012-01-01';
+set @range_end= '2012-02-02';
 set @range_size= datediff(date(@range_end),date(@range_begin));
 select @range_size;
 
@@ -32,7 +35,9 @@ where (((b.close_date - interval 1 day) between '2012-01-01' and '2012-02-01')
 and b.close_date>b.open_date;
 
 
-select a.Date, (select count(b.*) from bugs b where (b.close_date - interval 1 day) =a.Date)
+set @range_begin = '2012-01-01';
+set @range_end= '2012-02-02';
+select a.Date, (select count(*) from bugs b where b.open_date<=a.Date and (b.close_date> a.Date or b.close_date is null)  )
 from (
     select date(@range_begin) + INTERVAL (a.a + (10 * b.a) + (100 * c.a)) DAY as Date
     from (select 0 as a union all select 1 union all select 2 union all select 3 union all select 4 union all select 5 union all select 6 union all select 7 union all select 8 union all select 9) as a
