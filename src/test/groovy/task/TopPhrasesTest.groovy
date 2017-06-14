432e23f5625dab33657e019fb228b24f307d4e94
ernesto.alvarado@oracle.com <ealvarad@slc02oyd.us.oracle.com>
2017-06-14 16:52:13 -0700

    frases lee caca

---
+++ src/test/groovy/task/TopPhrasesTest.groovy
@@ -0,0 +1,20 @@
+package task
+
+import spock.lang.Specification
+import java.sql.*;
+import groovy.sql.Sql
+import tasks.TopPhrases
+
+class TopPhrasesTest extends Specification{
+	def 'ola'() {
+		given:
+		List<String> res=null;
+
+		when:
+		res=TopPhrases.findTopPhrases("/tmp/ass.txt",5)
+		println("oie zhiii "+res)
+
+		then:
+		true
+	}
+}
