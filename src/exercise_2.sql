8ea3a2141c31359ae3830753cf5c49ee1a93dda6
Darkly Dreaming Wesker <weskerhluffy@gmail.com>
2017-06-18 19:49:01 -0500

    documentadas rankeo de votos i initcap

--- src/exercise_2.sql
+++ src/exercise_2.sql
@@ -1,3 +1,9 @@
+-- Change the first letter of every word to uppercase. In this case a word is 
+-- defined as a set of characters which start with an alphabetic character and
+-- is delimited by empty space.
+-- Parameters:
+-- input_str IN The string whose words are to be capitalized on its first
+--              letter.
 drop function if exists initcap;
 delimiter $$
 create function initcap(input_str text)
@@ -8,17 +14,23 @@ begin
   declare spaces_present int;
   declare char_to_replace varchar(1);
   
+  -- For simplicity, remove leading and ending spaces.
   set input_str_int=trim(lower(input_str));
   set input_str_len=length(input_str_int);
   
+  -- If the string does not contain any alphanumeric character, there is
+  -- nothing to do to it.
   if(input_str_len = 0 or (input_str REGEXP '[[:alpha:]]*') = 0) then
     return input_str;
   end if;
   
---  set spaces_present=instr(input_str_int, ' ');
---  while (spaces_present <> 0) do
+  -- Traverse the string by jumping to every white space occurrence, on each
+  -- space check if the following character is alphabetic, if so, capitalize
+  -- it and jump to the next space, else, only look for the next white space.
+  -- Repeat this until there are no more spaces.
   set spaces_present=0;
   repeat
+    -- Also make sure we don't exceed the length of the string.
     if(spaces_present+1 <= input_str_len) then
       set char_to_replace = substring(input_str_int, spaces_present+1,1);
       if((char_to_replace REGEXP '[[:alpha:]]') <> 0) then
@@ -31,7 +43,6 @@ begin
     end if;
   until spaces_present = 0
   end repeat;
---  end while;
   
   return input_str_int;
 end;
