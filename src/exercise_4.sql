b5bf777300f92f849a1966c9745318818e715214
Darkly Dreaming Wesker <weskerhluffy@gmail.com>
2017-06-18 23:33:07 -0500

    documentada el range de bugs

--- src/exercise_4.sql
+++ src/exercise_4.sql
@@ -28,6 +28,26 @@ insert into bugs values (NULL, '2012-05-21','2012-09-16',10); -- 0
 insert into bugs values (NULL, '2012-10-05','2012-12-17',10); -- 0
 
 
+-- The main problem with this task is that there is no out-of-the-box way to
+-- generate a series of anything (like generate_series in postgreSQL), so that
+-- I could generate all dates of a given date range. So, to circunvent this, I
+-- generated all combinations of 0-9 from units to hundreds (000-999), one in
+-- each row, and in the select part of the query I added them (as days) to the
+-- begin date of the range wanted (this of course is assuming the range wanted
+-- is composed of less than a thousand days, but if that is the case we can
+-- generate the combinations up to 9999 or more as needed). 
+--
+-- After that I filtered only the window of interest (from the begin of the
+-- range to the end). This way I generated all the dates in the date range.
+--
+-- In the outer select I did a subselect (which is executed for each date in
+-- the range) to count all bugs that were opened in the current date or before
+-- and that ended the day after the current date or have not close date.
+--
+-- In this case I used mysql variables to centralize the parameters of the
+-- query (begin date, end date), but in a programming language this is easily
+-- traduced to a prepared statement and the dates can be the paremeters of
+-- such statement.
 set @range_begin = '2011-11-12';
 set @range_end= '2012-04-26';
 select a.Date as date, (select count(*) from bugs b where b.open_date<=a.Date and (b.close_date> a.Date or b.close_date is null)  ) as open_bugs
